376c54fd5fb445506cd78e0fccb750c165a4906c
Brian LeMaster <brian.lemaster@outlook.com>
2023-01-15 20:05:40 -0500

    Added roughed out Views in WPF project, added styles to App.xaml

---
+++ Client/TaskList2.UI.WPF/Views/Folders/AddUpdateFolderViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskList2.UI.WPF.Views.Folders
+{
+    internal class AddUpdateFolderViewModel
+    {
+    }
+}
